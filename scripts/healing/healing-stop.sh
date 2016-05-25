f3ab27e0b14f5f011e9f44a794db8b8f107503f1
alons <alons@gigaspaces.com>
2016-05-25 16:16:52 +0300

    add crontab removal on server stop

--- scripts/healing/healing-stop.sh
+++ scripts/healing/healing-stop.sh
@@ -1,5 +1,17 @@
-#! /bin/bash 
-read PID < /tmp/pid_file
+#! /bin/bash
+DPLID=$(ctx deployment id)
+read PID < /tmp/pid_file_${DPLID}
 kill -9 $PID
+rm /tmp/pid_file_${DPLID}
 
+# cleanup cron
+crontab_file=/tmp/mycron
+currVenv=/root/${DPLID}/env
 
+sudo crontab -l | grep -v ${currVenv} > ${crontab_file}
+sudo crontab ${crontab_file}
+
+status_code=$?
+ctx logger info "crontab ${crontab_file} status code is ${status_code}"
+currCrontab=`sudo crontab -l`
+ctx logger info "currCrontab is ${currCrontab}"
