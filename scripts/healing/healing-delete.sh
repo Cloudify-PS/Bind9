d9631723ff5b51190ee7629186bc357bb9c48f94
Anna Szpoton <anna.szpoton@gigaspaces.com>
2016-06-14 19:44:33 +0200

    Add remove created folders

--- scripts/healing/healing-delete.sh
+++ scripts/healing/healing-delete.sh
@@ -1,23 +1,14 @@
 DEPLOYMNET_ID=$(ctx deployment id)
+
 CONFIG_DIR="/etc/healing"
-SCRIPT_BASE="/root"
-
-
-SCRIPT_DIR="${SCRIPT_DIR}/${DEPLOYMNET_ID}"
-
-
-HEALING_SCRIPT="${SCRIPT_DIR}/healing.py"
-
-currVenv="${SCRIPT_DIR}/env"
-
+SCRIPT_DIR="/root/${DEPLOYMNET_ID}"
 CONFIG_FILE_NAME="healing_dog_${DEPLOYMNET_ID}"
-
 CONFIG_FILE="${CONFIG_DIR}/${CONFIG_FILE_NAME}"
+CRON_JOB="/etc/cron.d/${CONFIG_FILE_NAME}"
 
+ctx logger info "Removing config dir: ${CONFIG_DIR}, cron job: ${CRON_JOB} and working directory ${SCRIPT_DIR}"
 
-rm /etc/cron.d/${CONFIG_FILE_NAME}
-
-rm ${CONFIG_FILE}
-
-rm ${HEALING_SCRIPT}
+rm ${CRON_JOB}
+rm -r ${CONFIG_DIR}
+rm -r ${SCRIPT_DIR}
 
