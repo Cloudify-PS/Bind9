389dbb4641722c9607bdf2c0683fbbcbe42606a2
szpotona <anna.szpoton@gigaspaces.com>
2016-06-17 15:37:12 +0200

    Change logging in healing. Parametrize file paths.

--- scripts/healing/healing-configure.sh
+++ scripts/healing/healing-configure.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 ctx logger info "Retrieving nodes_to_monitor and deployment_id"
 DEPLOYMENT_ID=$(ctx deployment id)
+LOG_FILE=$(ctx node properties log_file)
+PID_FILE=$(ctx node properties pid_file)
+COOLDOWN_FILE=$(ctx node properties cooldown_file)
 SCRIPT_DIR="/root/${DEPLOYMENT_ID}"
 CONFIG_FILE="/etc/healing/healing_dog_${DEPLOYMENT_ID}"
 HEALING_SCRIPT="${SCRIPT_DIR}/healing.py"
 
 pythonPath=$(which python)
 NODES_TO_MONITOR="$(ctx instance runtime_properties nodes_to_monitor_path)"
-COMMAND="${pythonPath} ${HEALING_SCRIPT} ${NODES_TO_MONITOR} ${DEPLOYMENT_ID}"
+COMMAND="${pythonPath} ${HEALING_SCRIPT} -n ${NODES_TO_MONITOR} -d ${DEPLOYMENT_ID} -l ${LOG_FILE} -c ${COOLDOWN_FILE} -p ${PID_FILE}"
 
 ctx logger info "writing config file - ${CONFIG_FILE}  : ${COMMAND}"
 echo "*/1 * * * * root ${COMMAND}" > ${CONFIG_FILE}
--- scripts/healing/healing.py
+++ scripts/healing/healing.py
@@ -1,25 +1,44 @@
-import sys
+import argparse
+import datetime
+import logging
+import os
+
 from cloudify_rest_client import CloudifyClient
 from influxdb.influxdb08 import InfluxDBClient
 from influxdb.influxdb08.client import InfluxDBClientError
-from os import utime
-from os import getpid 
-from os import path
-import datetime
 
 # check against influxdb which nodes are available CPUtotal
 # autoheal only missing nodes comparing to the node_instances that are taken from cloudify
 # Checking it only for compute nodes
 
-COOL_DOWN_PATH = '/tmp/cooldown'
-LOG_FILE_PATH = '/tmp/logfile.log'
-PID_FILE_PATH = '/tmp/pid_file'
+
+def _parse_args():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('-l',
+                        '--logfile',
+                        required=True,
+                        metavar='<log file path>')
+    parser.add_argument('-d',
+                        '--deployment-id',
+                        metavar='<deployment id>')
+    parser.add_argument('-n',
+                        '--nodes-to-monitor',
+                        metavar='<nodes to monitor file path>')
+    parser.add_argument('-c',
+                        '--cooldown-file',
+                        metavar='<cooldown file>')
+    parser.add_argument('-p',
+                        '--pid-file',
+                        metavar='<pid file>')
+
+    return parser.parse_args()
 
 
-def cool_down():
-    if path.isfile(COOL_DOWN_PATH):
+def cool_down(cool_down_path):
+    if os.path.isfile(cool_down_path):
         now = datetime.datetime.now()
-        then = datetime.datetime.fromtimestamp(path.getmtime(COOL_DOWN_PATH))
+        then = datetime.datetime.fromtimestamp(
+            os.path.getmtime(cool_down_path))
         time_delta = now - then
         seconds = time_delta.total_seconds()
         if seconds < 420:
@@ -29,49 +48,66 @@ def cool_down():
     return False
 
 
-def check_heal(nodes_to_monitor, deployment_id):
-    if cool_down():
-        print('Exiting from check_heal...\n')
+def check_heal(nodes_to_monitor, deployment_id, cool_down_path, logger):
+    if cool_down(cool_down_path):
+        logger.info('Exiting from check_heal...')
         exit(0)
-    influx_client = InfluxDBClient(host='localhost', port=8086, database='cloudify')
-    log_file = open(LOG_FILE_PATH, 'w')
-    log_file.write('In check_heal\n')
+    logger.info('In check_heal. Getting clients.')
+    influx_client = InfluxDBClient(host='localhost',
+                                   port=8086,
+                                   database='cloudify')
     cloudify_client = CloudifyClient('localhost')
     # compare influx data (monitoring) to Cloudify desired state
 
     for node_name in nodes_to_monitor:
         for instance_id in nodes_to_monitor[node_name]:
-            log_file.write("deployment_id is {0}\n".format(deployment_id))
-            log_file.write("node_name is {0}\n".format(node_name))
-            log_file.write("instance.id is {0}\n".format(instance_id))
+            logger.info("Deployment_id: %s, node_name: %s, instance_id: %s "
+                        % (deployment_id, node_name, instance_id))
             q_string = 'SELECT MEAN(value) FROM /' + deployment_id + '\.' + node_name + '\.' + instance_id + '\.cpu_total_system/ GROUP BY time(10s) '\
                      'WHERE  time > now() - 40s'
-            log_file.write('query string is:{0}\n'.format(q_string))
+            logger.info('query string is:{0}'.format(q_string))
             try:
                 result = influx_client.query(q_string)
-                log_file.write('Query result is {0} \n'.format(result))
+                logger.info('Query result is {0} \n'.format(result))
                 if not result:
-                    log_file.write("Opening {0} and closing it\n".format(COOL_DOWN_PATH))
-                    open(COOL_DOWN_PATH, 'a').close()
-                    log_file.write("utime {0}\n".format(COOL_DOWN_PATH))
-                    utime(COOL_DOWN_PATH, None)
-                    log_file.write("Healing {0}\n".format(instance_id))
+                    logger.info("Opening {0} and closing it\n".format(
+                        cool_down_path))
+                    open(cool_down_path, 'a').close()
+                    logger.info("utime {0}\n".format(cool_down_path))
+                    os.utime(cool_down_path, None)
+                    logger.info("Healing {0}\n".format(instance_id))
                     execution_id = cloudify_client.executions.start(deployment_id, 'heal', {'node_instance_id': instance_id})
-                    log_file.write('execution_id is {0}\n'.format(str(execution_id)))
+                    logger.info('execution_id is {0}\n'.format(str(execution_id)))
             except InfluxDBClientError as ee:
-                log_file.write('DBClienterror {0}\n'.format(str(ee)))
-                log_file.write('instance id is {0}\n'.format(str(instance_id)))
+                logger.info('DBClienterror {0}\n'.format(str(ee)))
+                logger.info('instance id is {0}\n'.format(str(instance_id)))
             except Exception as e:
-                log_file.write(str(e))
+                logger.info(str(e))
 
 
-def main(argv):
-    pid_file = open(PID_FILE_PATH, 'w')
-    pid_file.write('%i' % getpid())
-    pid_file.close()
-    for i in range(len(argv)):
-        print("argv={0}\n".format(argv[i]))
-    with open(argv[1]) as f:
+def _set_logger(args):
+    logger = logging.getLogger('HEALING')
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(args.logfile)
+    fh.setLevel(logging.DEBUG)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    fh.setFormatter(formatter)
+    # add the handlers to logger
+    logger.addHandler(fh)
+    return logger
+
+
+def main():
+    args = _parse_args()
+    logger = _set_logger(args)
+    pid_file_path = args.pid_file
+    cool_down_path = args.cooldown_file
+    with open(pid_file_path, 'w') as pid_file:
+        pid_file.write('%i' % os.getpid())
+    with open(args.nodes_to_monitor) as f:
         lines = filter(None, f.read().split('\n'))
         nodes_to_monitor = {}
         for line in lines:
@@ -79,10 +115,11 @@ def main(argv):
             nodes = nodes_to_monitor.get(node_name, [])
             nodes.append(instance_id)
             nodes_to_monitor[node_name] = nodes
-
-    deployment_id = argv[2]
-    print("nodes={0}\n".format(nodes_to_monitor))
-    check_heal(nodes_to_monitor, deployment_id)
+    logger.info('Nodes to monitor: %s' % str(nodes_to_monitor))
+    check_heal(nodes_to_monitor,
+               args.deployment_id,
+               cool_down_path,
+               logger)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
